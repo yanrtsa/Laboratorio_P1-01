99851a61535ac0f80badfac941fe3d3a3283a1ed
yan.sa <fryhh44@gmail.com>
2026-02-22 22:13:31 -0300

    so colocando mais 1 comentarios mesmo

--- LAB_P1-01/attention.py
+++ LAB_P1-01/attention.py
@@ -15,7 +15,7 @@ def scaled_dot_product_attention(q,k,v) -> np.ndarray:
     d_k = k.shape[1] # Pegando o numero de colunas de K
     scores = np.matmul(q, k.T) # Multiplicacao de Q vezes a transposta de K
     scaled_scores = scores / np.sqrt(d_k) # Pegando a multiplicação e dividindo pela raiz do numero de colunas de K
-    attention_weights = softmax(scaled_scores)
+    attention_weights = softmax(scaled_scores) # Normaliza os valores de 0 a 1
     output = np.matmul(attention_weights, v)
 
     return output
